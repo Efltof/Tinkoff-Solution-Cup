b5c760646b77e9ac31c6c70e9ab0d9f85ac73f7a
Alexander Kondrashin <a.kondrashin@tinkoff.ru>
2023-04-22 12:40:16 +0300

    Add design system framework

---
+++ TSCDesignSystem/Components/Header/TSCHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aleksandr Kondrashin on 22.04.2023.
+//
+
+import Foundation
+import UIKit
+
+public final class TSCHederView: UIView {
+    
+}
---
+++ TSCDesignSystem/Sources/TSCDesignSystem/TSCDesignSystem.swift
@@ -0,0 +1,6 @@
+public struct TSCDesignSystem {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
