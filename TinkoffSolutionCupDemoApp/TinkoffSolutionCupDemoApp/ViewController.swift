808537359b64ea3d332eb9f9196bc29206316493
Alexander Kondrashin <a.kondrashin@tinkoff.ru>
2023-04-22 12:32:14 +0300

    Demo app create

---
+++ TinkoffSolutionCupDemoApp/TinkoffSolutionCupDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffSolutionCupDemoApp
+//
+//  Created by Aleksandr Kondrashin on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
